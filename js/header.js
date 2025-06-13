8e1bdf118f5ca754fdb0c881571ad5c6b16743a5
PalMz <marcus.xbox.360@hotmail.com>
2025-06-13 22:28:23 +0200

    feat(header): dynamically switch HTML and more
    Dynamically fetch and load HTML files based on media screen width.
    - Improve routines for fetching, inserting and caching HTML data
    - Add a registry of inserted HTML data for preventing duplicate DOM
    manipulations and event listeners
    - Add "headerOnDOMInsert" function for doing work on header after
    inserting data into the DOM
    - Fix bugs in the modular promise chains
    - Add error handling to all promise chains for easier debugging
    - Refactor code and comments for improved readability and
    maintainability
    - Fix spelling errors

--- js/header.js
+++ js/header.js
@@ -7,16 +7,24 @@
 // Server prefix URL kept in one place for appending filepaths easily 
 const serverBaseURL = "http://localhost:8000"
 
+// Resource URLs
+const headerNarrowHTMLURL = serverBaseURL + "/source/collectors-store/html/header_narrow.html";
+const headerWideHTMLURL = serverBaseURL + "/source/collectors-store/html/header_wide.html";
+
 // Map for keeping track of fetched data used for dynamic DOM manipulation
 // Structure: URL<string>, { data:value<string>, inserted:value<boolean> }
-let fetchedDataMap = new Map();
+const fetchedDataMap = new Map();
+
+// Set for keeping track of inserted data in this script scope
+/** @type {Set<URL>} */
+const insertedDataSet = new Set();
 
 // DOM objects
-let bodyElement;
 let searchButtonMobile;
+let searchButtonContainerMobile;
 
 // Media Query List for narrow screens
-let MediaQueryListNarrow;
+let mediaQueryListNarrow;
 
 
 ///////////////////////////////////////////
@@ -36,13 +44,10 @@ function handleDOMContentLoaded() {
 
 
 /////////////////////////////////////////////////////////////
-//  Routine for switching HTML structure based on platform 
+//  Routine for switching HTML structure based on media
 /////////////////////////////////////////////////////////////
 
 function initializePlatformSwitch() {
-    // Get <body> element as a target for inserting content
-    bodyElement = document.querySelector("body");
-
     // Create MediaQueryList object for narrow screen
     mediaQueryListNarrow = matchMedia(
         "(width < 900px) or (orientation: portrait)"
@@ -61,91 +66,140 @@ function handleResize(event) {
 
 // Switch header component for wide and narrow screen version
 function switchHeaderHTML() {
-    // Check MediaQueryList object
-    let relativePath;
+    // Check with MediaQueryList object if the screen size is narrow
+    let url;
     if (mediaQueryListNarrow.matches) {
-        relativePath = "/source/collectors-store/html/header_narrow.html";
+        url = headerNarrowHTMLURL;
     }
     else {
-        relativePath = "/source/collectors-store/html/header_wide.html";
+        url = headerWideHTMLURL;
     }
 
-    // Check if required data already is inserted into the DOM
+    // Check if the required data is already inserted into the DOM
+    if (insertedDataSet.has(url)) {
+        return;
+    } 
 
-
-    // Check if required data is already cached in this script context
-    const url = serverBaseURL + relativePath;
+    // Check if required data is already cached in this script scope    
     if (fetchedDataMap.has(url)) {
         // Then get the cached data for next step
-        const dataRecord = fetchedDataMap.get(url);
+        const fetchRecord = fetchedDataMap.get(url);
 
-        // Check if the required data is already inserted in the DOM
-        if (dataRecord.inserted) {
-            return;
-        } else {
-            // Otherwise, insert the required data into the DOM
-            insertHTML(dataRecord.data);
-        }
+        // Insert the cached HTML data
+        insertHTML(fetchRecord, document.body, "div", url)
+        .then(headerOnDOMInsert)
+        .catch((error) => console.error(`switchHeaderHTML - Caught error in Promise chain after insertHTML: ${error}`));
     } else {
         // Otherwise, fetch the data and then insert into the DOM
-        fetchHTMLAndInsert(url);
+        fetchHTMLAndInsert(url)
+        .then(headerOnDOMInsert)
+        .catch((error) => console.error(`switchHeaderHTML - Caught error in Promise chain after fetchHTMLandInsert: ${error}`));
     }
 }
 
 function fetchHTMLAndInsert (url) {
     if (url && typeof url === "string") {
-        fetchHTML(url)
+        return fetchHTML(url)
         .then((content) => {
-            insertHTML(content, bodyElement, "div")
-            return Promise.resolve(true);
+            insertHTML(content, document.body, "div", url)
+            .catch((error) => {
+                console.error(`fetchHTMLAndInsert - Caught error while inserting HTML: ${error}`)
+            });
         })
         .catch((error) => {
-            console.error(`fetchHTMLAndInsert - Caught error while fetching and inserting HTML data: ${error}`)
+            return Promise.reject(`fetchHTMLAndInsert - Caught error while fetching and inserting HTML data: ${error}`);
         });
     } else {
-        console.error("fetchHTMLAndInsert - HTMLFileName has a falsy value or is not a string type");
+        return Promise.reject("fetchHTMLAndInsert - HTMLFileName has a falsy value or is not a string type");
     }
+    // Fallback return for any potential future coding mistakes, to make debugging easier
+    return Promise.reject("fetchHTMLAndInsert - fallback return. This should not fire");
 }
 
 function fetchHTML (url) {
     return fetch(url)
         .then((response) => {
             if (response.ok) {
-                return response.text();    // TODO: Validation for HTML content
+                return response.text();
+                //return Promise.resolve(response.url, response.text());
             }
             else {
-                throw new Error(`HTTP error - status code: ${response.status}`);
+                Promise.reject(`HTTP error - status code: ${response.status}`);
             }
+        })
+        .then((content) => {
+            // Cache the fetched HTML content
+            // Warning: responseURL may be altered by redirects, potentially different from sourceURL
+            //fetchedDataMap.set(responseURL, content);
+
+            // Return back to outer promise chain with resolve promise
+            return Promise.resolve(content);
         })
         .catch((error) => {
             console.error(`fetchHTML - Caught error while fetching HTML data: ${error}`);
         })
 }
 
+/** @param {string} content
+* @param {object} targetElement
+* @param {string} containerType - (optional)
+* @param {string} sourceURL
+* @param {boolean} doReplace
+*/
 function insertHTML (content, targetElement, containerType, sourceURL) {
     // Validate mandatory arguments
     if (!(content && targetElement && sourceURL)) {
-        throw new Error("insertHTML function received mandatory arguments with at least one falsy value");
+        // An outer catch promise is expected to handle this throwing error
+        return Promise.reject("insertHTML - mandatory arguments have at least one falsy value");
     }
 
     // Check if the optional argument containerType is specified in this function call
     if (typeof containerType !== "undefined") {
         
         // Wrap the HTML content by creating a container with specfied type
-        const container = document.createElement(containerType);
-        container.innerHTML = content;
+        //const container = document.createElement(containerType);
+        //container.innerHTML = content;
+        const wrappedContent = `<${containerType}>${content}</${containerType}>`;
 
         // Insert the wrapped HTML content into the target element
-        targetElement.appendChild(container);
+        targetElement.innerHTML = wrappedContent;
 
     } else {
         // Otherwise, just insert the HTML content directly into the target element
-        targetElement.appendChild(content);
+        targetElement.innerHTML = content;
     }
 
-    // Register the inserted HTML content and that it's currently used in the DOM
-    fetchedDataMap.set(sourceURL, { data:content, inserted:true });
+    // Cache the fetched HTML content
+    fetchedDataMap.set(sourceURL, content);
 
+    // Return back to outer promise chain with resolve promise
+    return Promise.resolve(sourceURL);
+}
+
+function headerOnDOMInsert(insertedURL) {
+    // Check if the mobile header HTML was currently inserted
+    if (insertedURL == headerNarrowHTMLURL) {
+        initializeSearchButtonMobile();
+    }
+
+    // Check if the mobile header HTML has been replaced by any other header representation
+    else if (insertedDataSet.has(headerNarrowHTMLURL)) {
+        searchButtonMobileOnRemove();
+
+        // Finally, delete it from the set
+        insertedDataSet.delete(headerNarrowHTMLURL);
+    }
+
+    // Check if the wide header HTML has been replaced by any other header representation
+    if (insertedURL != headerWideHTMLURL && insertedDataSet.has(headerWideHTMLURL)) {
+        // Then delete it from the set
+        insertedDataSet.delete(headerWideHTMLURL);
+    }
+
+    // Finally, after using the set of inserted data, add the currently inserted URL into the set
+    insertedDataSet.add(insertedURL);
+
+    // Return outer promise chain with a resolve promise
     return Promise.resolve(true);
 }
 
@@ -154,29 +208,46 @@ function insertHTML (content, targetElement, containerType, sourceURL) {
 //  Routine for search button in mobile version
 ///////////////////////////////////////////////////
 
+// Routine needs to be run everytime the mobile representation of the header component is loaded
 function initializeSearchButtonMobile() {
     searchButtonMobile = document.querySelector("#nav-search-button.mobile");
-    
-    // Attach event listener to search button in mobile version
+    searchButtonContainerMobile = document.querySelector("#nav-search-bar-container");
+
+    // Validate the mobile search button element variable, otherwise, do nothing. TODO: improve validation
     const searchButtonType = typeof searchButtonMobile;
-    if (searchButtonType !== "undefined" && searchButtonType !== "null" && searchButtonMobile != null) {  // TODO: better validation
-        searchButtonMobile.addEventListener("click", handleSearchMobileClick);
+    if (typeof searchButtonMobile !== "undefined" && typeof searchButtonMobile !== "null" && searchButtonMobile != null) {
+        // Check if the mobile search button mobile element is currently inserted into the DOM
+        if (document.body.contains(searchButtonMobile)) {
+            // Attach event listener to search button in mobile version
+            searchButtonMobile.addEventListener("click", handleSearchClickMobile);
+        } else {
+            console.error("initializeSearchButtonMobile - Expected mobile search button element to be a descendant of body element, but returned false");
+        }
+    } else {
+        console.error("initializeSearchButtonMobile - Expected to find mobile search button element, but failed");
     }
 }
 
+function searchButtonMobileOnRemove() {
+    searchButtonMobile.removeEventListener("click", handleSearchClickMobile);
+}
+
 // Define event handler for search button in mobile version
-function handleSearchMobileClick(event) {
-    if (searchButtonMobile) {
-        const display = searchButtonMobile.style.display;
+function handleSearchClickMobile(event) {
+    if (searchButtonContainerMobile) {
+        const display = searchButtonContainerMobile.style.display;
 
         // Switch between "block" and "none" values from CSS property "display"
         // Background: HTML and CSS is already set up statically in files
         switch (display) {
             case "none":
-                searchButtonMobile.style.display = "block";
+                searchButtonContainerMobile.style.display = "block";
                 break;
             case "block":
-                searchButtonMobile.style.display = "none";
+                searchButtonContainerMobile.style.display = "none";
+                break;
+            default:
+                searchButtonContainerMobile.style.display = "block";
         }
     } else {
         console.error("searchButtonMobile reference variable has a falsy value");
