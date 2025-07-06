13f683a3a7268fd6300e51015a8f35b90ec9bce2
PalMz <marcus.xbox.360@hotmail.com>
2025-07-06 23:12:14 +0200

    feat(grid): API response headers & implementation
    Changes in the API of the Product Grid, for communication between client
    and server
    
    - Adapt to the rename of filename "product_grid_response_sample.json" in
    frontend
    - Add "isLastChunk" response header. The server is expected to inform
    the client when the last results have been sent for the given query
    (reached the end). To be implemented.
    - Add a name "productEntryArray" to the unnamed response header holding
    an array of data structured for the "Product Entry" feature
    - Implement the "productEntryArray" response header in frontend

--- js/product_grid.js
+++ js/product_grid.js
@@ -1,65 +1,60 @@
 "use strict";
 
-// Element references
-const container = document.body.querySelector(".container");
+////////////////////////////////////////
+// Modules
+////////////////////////////////////////
 
-// Wait for Product Entry script to evaluate before proceeding
+// Wait for Product Entry script to evaluate before proceeding. It will define a Custom Element in the registry, register Event Listeners and load its dependencies
 import "/source/collectors-store/js/product_entry.js";
 
+
+////////////////////////////////////////
+// Script-level variables
+////////////////////////////////////////
+
+/** Flag for when the server responds with the last chunk of Product Entries for a given query
+ *  @type {Boolean} */
+let isLastChunk;
+
+// Grid container that will hold Product Entries
+const grid = document.body.querySelector(".container");
+
+
+////////////////////////////////////////
+// Main functionality
+////////////////////////////////////////
+
+// Ready to run one iteration of propagating the grid
 propagateGrid();
 
-// Deprecated
-function insertProductEntryJS() {
-    fetch("/source/collectors-store/html/product_entry.js")
-    .then((response) => {
-        if (!response.ok) console.error("Product Grid - function insertProductEntryJS: Response HTTP status code is not OK");
-        return response.text();
-    })
-    .then((JSText))
-}
 
-// Deprecated
-function placeholderFunc () {
-    return fetch("/source/collectors-store/html/product_entry.html", { cache:"default", mode:"same-origin" })
-    .then((response) => {
-        if (!response.ok) console.error("Response status not ok");
-        return response.text();
-    })
-    .then((HTMLText) => {
-        for (let i = 0; i < 12; i++) {
-            // Take HTML Data
-            // Create wrapper element
-            const wrapper = document.createElement("div");
-            // Insert html text
-            wrapper.innerHTML = HTMLText;
-            // Append wrapper to container
-            container.appendChild(wrapper);
-            // Create script element
-            const script = document.createElement("script");
-            script.src = "/source/collectors-store/js/product_entry.js";
-            // Append script to wrapper
-            wrapper.appendChild(script);
-        }
-    });
-}
-
+// Propagate the grid container with one or more instances of "Product Entry" feature. Can be called multiple times for increasing the number of entries
 function propagateGrid() {
-    fetch("/source/collectors-store/json/product_entry_data_chunk_sample.json")
+    return fetch("/source/collectors-store/json/product_grid_response_sample.json")
     .then((response) => {
         if (!response.ok) console.error("Product Grid - function propagateGrid: Response HTTP status code is not OK");
         return response.text();
     })
     .then((JSONText) => {
-        const arrayOfObjects = JSON.parse(JSONText);
-        for (const entry of arrayOfObjects) {
-            // TODO:
-            // Get entry
-            // Create Custom Element
-            // Add Custom Element into grid "container"
-            // Pass entry data as text into Custom Element attribute as "product-entry-data". JSON.stringify()
+        // Expect "Product Grid Response" structure:
+        // "isLastChunk": <boolean>. The server is expected to inform the client when the last products has been send for the given query
+        // "productEntryArray": <array: <productEntryResponse>>. An array with one or more objects containing data for the Product Entry feature 
 
+        // Parse the JSON text into JS objects
+        const productGridResponse = JSON.parse(JSONText);
+
+        // TODO Validate properties from structure above
+        if (typeof productGridResponse.isLastChunk === "undefined" || typeof productGridResponse.productEntryArray === "undefined") return Promise.reject("Product Grid - function propagateGrid: Product Grid Response is invalid");
+
+        isLastChunk = productGridResponse.isLastChunk;  // TODO: If true, display a hint "End of results" on screen
+
+        // Loop through the Product Entry Array (size is determined by server)
+        for (const entry of productGridResponse.productEntryArray) {
+            // Create and add a Custom Element to the grid container
             const productEntry = document.createElement("product-entry");
-            container.appendChild(productEntry);
+            grid.appendChild(productEntry);
+
+            // Create and add an attribute node for passing one "Product Entry Data Structure" as string type to the "Product Entry". This is expected to fire the "attributeChangedCallback" function on the Custom Element
             const attr = document.createAttribute("product-entry-data");
             attr.value = JSON.stringify(entry);
             productEntry.setAttributeNode(attr);
